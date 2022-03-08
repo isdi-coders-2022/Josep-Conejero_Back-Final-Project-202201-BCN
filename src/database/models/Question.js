d1335a746e21a39f53abfc870a04497064d6f7f5
Josep Conejero <bermeu@gmail.com>
2022-03-08 14:26:47 +0100

    Added ./src/database/models/Question.js

---
+++ src/database/models/Question.js
@@ -0,0 +1,22 @@
+const { model, Schema } = require("mongoose");
+
+const QuestionSchema = new Schema({
+  question: {
+    type: string,
+    default: Date.now,
+  },
+  text: {
+    type: String,
+    required: true,
+    min: 1,
+    max: 200,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Question = model("Question", QuestionSchema, "questions");
+
+module.exports = Question;
