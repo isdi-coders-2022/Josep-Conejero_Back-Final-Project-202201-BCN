406d253951d8e50954312b35e612389d4eb8fc23
Josep Conejero <bermeu@gmail.com>
2022-03-17 22:22:14 +0100

    Added /questionsList to ./src/server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
 const questionRouter = require("./routers/questionRouter");
+const questionsListRouter = require("./routers/questionsListRouter");
 
 const { notFoundError, generalError } = require("./middlewares/errors");
 
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use(helmet());
 
 app.use("/question", questionRouter);
+app.use("/questionsList", questionsListRouter);
 
 app.use(notFoundError);
 app.use(generalError);
