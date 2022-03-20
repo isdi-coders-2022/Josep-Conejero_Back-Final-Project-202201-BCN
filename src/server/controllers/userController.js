92626ae35b72d3e644e1ea5607b56c2b17fbce74
Josep Conejero <bermeu@gmail.com>
2022-03-20 21:34:44 +0100

    Modified lf in userController.js

--- src/server/controllers/userController.js
+++ src/server/controllers/userController.js
@@ -58,17 +58,6 @@ const loginUser = async (req, res, next) => {
   return res.json({ token });
 };
 
-/* const getAllUsers = async (req, res) => {
-  const headerAuth = req.header("Authorization");
-  const token = headerAuth.replace("Bearer ", "");
-  const { id } = jwt.verify(token, process.env.JWT_SECRET);
-
-  const users = await User.find();
-  const actualUser = await User.findById(id);
-  const returnedUsers = users.filter((user) => user.id !== actualUser.id);
-  res.status(200).json({ returnedUsers });
-}; */
-
 const getUser = async (req, res) => {
   const headerAuth = req.header("Authorization");
   const token = headerAuth.replace("Bearer ", "");
@@ -79,55 +68,3 @@ const getUser = async (req, res) => {
 };
 
 module.exports = { registerUser, loginUser, getUser };
-
-/* const bcrypt = require("bcrypt");
-const jsonwebtoken = require("jsonwebtoken");
-const User = require("../../database/models/User");
-const encryptPassword = require("../utils/encryptPassword");
-
-const registerUser = async (req, res, next) => {
-  const user = req.body;
-  const { username, password } = user;
-
-  const existingUser = await User.findOne({ username });
-
-  if (!existingUser) {
-    const encryptedPassword = await encryptPassword(password);
-    user.password = encryptedPassword;
-    const createdUser = await User.create(user);
-    res.json(createdUser);
-  } else {
-    const error = new Error("Sorry, username already taken");
-    error.code = 409;
-    next(error);
-  }
-};
-
-const loginUser = async (req, res, next) => {
-  const { username, password } = req.body;
-  const user = await User.findOne({ username });
-
-  if (!user) {
-    const error = new Error("Username or password are wrong");
-    error.code = 403;
-    next(error);
-  } else {
-    const userData = {
-      name: user.name,
-      username: user.username,
-      id: user.id,
-    };
-    const rightPassword = await bcrypt.compare(password, user.password);
-    if (!rightPassword) {
-      const error = new Error("Username or password are wrong");
-      error.code = 403;
-      next(error);
-    } else {
-      const token = jsonwebtoken.sign(userData, process.env.JWT_SECRET);
-      res.json({ token });
-    }
-  }
-};
-
-module.exports = { registerUser, loginUser };
- */
